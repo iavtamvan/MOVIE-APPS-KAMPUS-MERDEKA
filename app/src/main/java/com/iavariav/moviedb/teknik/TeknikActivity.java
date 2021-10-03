fd7e859701be3f4a19889b41b1f665883bc66656
Ade Fajr Ariav <iav@192.168.1.7>
2021-10-03 19:12:00 +0700

    adding get Teknik and Sharedpref

--- app/src/main/java/com/iavariav/moviedb/teknik/TeknikActivity.java
+++ app/src/main/java/com/iavariav/moviedb/teknik/TeknikActivity.java
@@ -70,6 +70,7 @@ public class TeknikActivity extends AppCompatActivity {
 
     }
 
+
     private void getDataTeknik() {
         ApiService apiService = ApiConfig.getApiService(Config.BASE_URL_WEBHOST_APP);
         apiService.ambilDataTeknik()
